12e8755723a8f7e95fcbccc3b47e7ac2004497c3
r-oberson <roberson@filmetrics.com>
2015-04-06 15:52:11 -0700

    SAM-BA 2.12 patch 6 -> 2.15 w/LPDDR2 patch
    Upgraded SAM-BA from version 2.12 patch level 6 (which didn't support LPDDR2) to
    SAM-BA 2.15 which includes LPDDR2 drivers (for the SI board).
    
    Also added a patch file that changes the RAM configuration of the SAMA5D3X Eval
    Kit (used for the SI board) from 16-bit DDR2 to 32-bit LPDDR2.

--- package/sam-ba/sam-ba.mk
+++ package/sam-ba/sam-ba.mk
@@ -5,17 +5,24 @@
 ################################################################################
 
 SAM_BA_SITE    = http://www.atmel.com/Images/
-SAM_BA_VERSION = 2.12
+SAM_BA_VERSION = 2.15
+## was: SAM_BA_VERSION = 2.12 -- 2015-04-06 RGO Filmetrics Inc.
 SAM_BA_SOURCE  = sam-ba_$(SAM_BA_VERSION).zip
-SAM_BA_PATCH = patch6.gz
+## was: SAM_BA_PATCH = patch6.gz -- 2015-04-06 RGO Filmetrics Inc.
 ## was: SAM_BA_PATCH = sam-ba_$(SAM_BA_VERSION)_patch5.gz
 SAM_BA_LICENSE = BSD-like (partly binary-only)
 SAM_BA_LICENSE_FILES = doc/readme.txt
 
 define HOST_SAM_BA_EXTRACT_CMDS
         unzip -d $(BUILD_DIR) $(DL_DIR)/$(SAM_BA_SOURCE)
-        mv $(BUILD_DIR)/sam-ba_cdc_cdc_linux/* $(@D)
-        rmdir $(BUILD_DIR)/sam-ba_cdc_cdc_linux/
+
+        # Between revisions 2.12 and 2.15, Atmel changed the top level directory
+        # in the zip archive from sam-ba_cdc_cdc_linux to sam-ba_cdc_linux.
+        # 2015-04-06 RGO Filmetrics Inc.
+        # Was : mv $(BUILD_DIR)/sam-ba_cdc_cdc_linux/* $(@D)
+        # Was : rmdir $(BUILD_DIR)/sam-ba_cdc_cdc_linux/
+        mv $(BUILD_DIR)/sam-ba_cdc_linux/* $(@D)
+        rmdir $(BUILD_DIR)/sam-ba_cdc_linux/
 endef
 
 # Since it's a prebuilt application and it does not conform to the
@@ -27,6 +34,10 @@ define HOST_SAM_BA_INSTALL_CMDS
 	mkdir -p $(HOST_DIR)/opt/sam-ba/
 	cp -a $(@D)/* $(HOST_DIR)/opt/sam-ba/
 	ln -sf ../../opt/sam-ba/sam-ba $(HOST_DIR)/usr/bin/sam-ba
+
+        # Version 2.15 has a sam-ba_64 executable also, so we link to it also.
+        # 2015-04-06 RGO Filmetrics Inc.
+        ln -sf ../../opt/sam-ba/sam-ba_64 $(HOST_DIR)/usr/bin/sam-ba_64
 endef
 
 $(eval $(host-generic-package))
